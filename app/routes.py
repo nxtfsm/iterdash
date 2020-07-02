9ffc6373f2f64c16e1b73ff9486ecabfb13508a9
nxtfsm <sysadmin@xaviermicronesia.org>
2020-07-01 20:28:02 -0500

    switch dashes

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,27 @@ from app import app
 @app.route('/index')
 def index():
     return render_template('index.html', title="Dashly Dash")
+
+@app.route('/Home')
+def dash_home():
+    return render_template('dash/home.html')
+
+@app.route('/Water')
+def dash_water():
+    return render_template('dash/water.html')
+
+@app.route('/Gardens')
+def dash_gardens():
+    return render_template('dash/gardens.html')
+
+@app.route('/Climate')
+def dash_climate():
+    return render_template('dash/climate.html')
+
+@app.route('/Media')
+def dash_media():
+    return render_template('dash/media.html')
+
+@app.route('/Network')
+def dash_network():
+    return render_template('dash/network.html')
--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,14 +1,12 @@
 $(document).ready( () => {
   animTests()
   elementLoader()
-
 })
 
 let elementLoader = () => {
   $(document).on('click', '.bar.topNav > button', function () {
-    document.querySelector('#testText').innerHTML = this.innerHTML
     updateActiveTabTo(this)
-
+    loadNewDash(this.innerHTML)
   })
 }
 
@@ -18,12 +16,35 @@ let clearActiveClass = () => {
   // Removes 'active' class tag from *all* elements where currently set
   Array.from(document.querySelectorAll('.active')).forEach((currentActive) => {
     toggleClass(currentActive, 'active')
-
   });
 }
 
 let toggleClass = (element, className) => element.classList.toggle(className)
 
-let animTests = () =>
+let loadNewDash = dashToLoad => loadDashHTML("/" + dashToLoad)
+
+let loadDashHTML = fromRoute => {
+  fetch(fromRoute)
+    .then( response => response.text() )
+      .then( (data) => {
+        data = data.trim()
+        $("#topGrid").append(data)
+      })
+
+}
+
+let animDashLoad = () => {
+
+  //gsap.to('#topGrid:')
+}
+
+let animTests = () => {
   //gsap.from('.bar.topNav', {duration: 1, y: '-100%', ease: 'sine'})
-  gsap.from('.dash-panel.test', {duration: 1, delay: 1, x: '-100%', y:'100%', opacity:0, ease: 'sine'})
+  //gsap.from('#testPanel', {duration: 1, delay: 1, x: '-100%', opacity:0, ease: 'sine'})
+  //gsap.from('#anotherTestPanel', {duration: 1, delay: 1, x: '-100%', opacity:0, ease: 'sine'})
+   //const tl = gsap.timeline({defaults: { duration: 1 }});
+  //tl
+  //  .from('#testPanel', {delay: 1, x: '-100%', opacity:0, ease: 'sine'})
+  //  .from('#anotherTestPanel', {delay: 1, x: '-100%', opacity:0, ease: 'sine'})
+  //return tl
+}
